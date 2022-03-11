15016bf542a369a9de4dda8417b9afc91ebfbdec
ignacioarroyo7 <ignacioarroyo17@gmail.com>
2022-03-11 20:38:51 -0300

    se realizo la estructura de la card

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import logo from './logo.png'; //importo en la variable logo el logo que esta en src/logo.png
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
+import Frase from './components/Frase';
 
 function App() {
   return (
     <section className='container my-5 d-flex flex-column align-items-center'>
       <img src={logo} alt="Logo de los simpsons" />
       <Button variant='warning' className='w-50 text-light my-5'>Obtener frase</Button>
+      <Frase></Frase>
     </section>
   );
 }
---
+++ src/components/Frase.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {Card} from 'react-bootstrap';
+
+const Frase = () => {
+  return (
+      <Card>
+        <Card.Body>
+            <div className="row">
+                <div className="col-sm-12 col-md-4">
+                    <img src="" alt="" />
+                </div>
+                <div className="col-sm-12 col-md-8">
+                    <Card.Title></Card.Title>
+                    <Card.Text>
+            
+                    </Card.Text>
+                </div>
+            </div>
+        </Card.Body>
+      </Card>
+  );
+};
+
+export default Frase;
