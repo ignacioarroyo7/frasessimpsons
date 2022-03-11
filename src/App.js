91c97f654b45a650f0e5d7a3986677a5e5ac9c39
ignacioarroyo7 <ignacioarroyo17@gmail.com>
2022-03-11 20:16:40 -0300

    limpie la estructura del proyecto e instale bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png'; //importo en la variable logo el logo que esta en src/logo.png
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className='container my-5 d-flex flex-column align-items-center'>
+      <img src={logo} alt="Logo de los simpsons" />
+      <Button variant='warning' className='w-50 text-light my-5'>Obtener frase</Button>
+    </section>
   );
 }
 
