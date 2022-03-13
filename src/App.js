7b0977c267f7770cf0b596de09ad4aebe7852bff
ignacioarroyo7 <ignacioarroyo17@gmail.com>
2022-03-13 19:04:26 -0300

    se agregregon los hooks state y effect, la funcion para consultar a la api. La logica para consultar a la api en fase de montaje y al presionar el boton

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import logo from './logo.png'; //importo en la variable logo el logo que esta en
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import Frase from './components/Frase';
+import {useState, useEffect} from "react";
 
 function App() {
+
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(()=>{
+    consultarAPI();
+  },[]);
+
+  const consultarAPI = async ()=>{
+    const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes') //fetch hace un request o una peticion a la url y se guarda en la variable respuesta
+    const dato = await respuesta.json();
+    console.log(respuesta);
+    console.log(dato[0]);
+    //guardar los datos dentro del state
+    setPersonaje(dato[0]);
+  }
+
   return (
     <section className='container my-5 d-flex flex-column align-items-center'>
       <img src={logo} alt="Logo de los simpsons" />
-      <Button variant='warning' className='w-50 text-light my-5'>Obtener frase</Button>
-      <Frase></Frase>
+      <Button variant='warning' className='w-50 text-light my-5' onClick={()=>consultarAPI()}>Obtener frase</Button>
+      <Frase personaje={personaje}></Frase>
     </section>
   );
 }
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,18 +1,18 @@
 import React from "react";
 import {Card} from 'react-bootstrap';
 
-const Frase = () => {
+const Frase = (props) => {
   return (
       <Card>
         <Card.Body>
             <div className="row">
                 <div className="col-sm-12 col-md-4">
-                    <img src="" alt="" />
+                    <img src={props.personaje.image} alt={props.personaje.character} />
                 </div>
                 <div className="col-sm-12 col-md-8">
-                    <Card.Title></Card.Title>
+                    <Card.Title>{props.personaje.character}</Card.Title>
                     <Card.Text>
-            
+                        {props.personaje.quote}
                     </Card.Text>
                 </div>
             </div>
